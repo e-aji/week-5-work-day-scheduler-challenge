1d9dd190ecbd8e24e3c1ab2d6cd4d3b29ec9cd63
eji_a <156595423+e-aji@users.noreply.github.com>
2024-02-18 16:31:38 +0000

    completed javascript code, made css and html changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+//HTML to load before function executed 
 $(document).ready(function () {
 
+  // When save button clicked, the users' input is saved in local storage for that time
   $('.saveBtn').on('click', function(){
     var value = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
@@ -17,6 +17,7 @@ $(document).ready(function () {
 
   });
 
+  // Function to assign a specific class to each block depending on what hour of day it is 
 function hourUpdater () {
 
   var currentHour = dayjs().hour();
@@ -34,13 +35,21 @@ function hourUpdater () {
     }  
   });
 }
+// Call the hourUpdater function when the document is ready
+
+$(document).ready(function () {
+
+  hourUpdater();
+});
+
+// Restore user input data saved in local storage and display on page in specific element when refreshed
 
 $('.description').each(function(){
 
   var time = $(this).parent().attr('id');
   $(this).val(localStorage.getItem(time));
  
-  }) 
+  }); 
 
  // Displays the current date in the header of the page.
  
