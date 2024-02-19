d7dde2846e86e17de5e41d6d1674a90fb70efeb0
eji_a <156595423+e-aji@users.noreply.github.com>
2024-02-19 18:46:22 +0000

    added notification code in HTML and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,60 +18,41 @@ $(document).ready(function () {
   });
 
   // Function to assign a specific class to each block depending on what hour of day it is 
-function hourUpdater () {
+  function hourUpdater () {
 
-  var currentHour = dayjs().hour();
-  $('.time-block').each(function() {
-    var blockHour = parseInt($(this).attr('id').split('-')[1]);
-    
-    if (blockHour < currentHour){
-    $(this).addClass('past');
+    var currentHour = dayjs().hour();
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+      
+      if (blockHour < currentHour){
+      $(this).addClass('past');
 
-    }else if (blockHour == currentHour){
-      $(this).addClass('present');
+      }else if (blockHour == currentHour){
+        $(this).addClass('present');
 
-    }else {
-    $(this).addClass('future');
-    }  
-  });
-}
-// Call the hourUpdater function when the document is ready
+      }else {
+      $(this).addClass('future');
+      }  
+    });
+  }
+  // Call the hourUpdater function when the document is ready
 
-$(document).ready(function () {
+    $(document).ready(function () {
 
-  hourUpdater();
+      hourUpdater();
+    });
+
+  // Restore user input data saved in local storage and display on page in specific element when refreshed
+
+  $('.description').each(function(){
+
+    var time = $(this).parent().attr('id');
+    $(this).val(localStorage.getItem(time));
+  
+    }); 
+
+  // Displays the current date in the header of the page.
+  
+  $('#currentDay').text(dayjs().format('dddd, MMM D, YYYY'));
+  
 });
-
-// Restore user input data saved in local storage and display on page in specific element when refreshed
-
-$('.description').each(function(){
-
-  var time = $(this).parent().attr('id');
-  $(this).val(localStorage.getItem(time));
- 
-  }); 
-
- // Displays the current date in the header of the page.
- 
- $('#currentDay').text(dayjs().format('dddd, MMM D, YYYY'));
-});
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
